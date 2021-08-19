be968855fb6081b050e42526457b93718e29569d
Edward484 <selea.eduard@gmail.com>
2021-08-19 17:13:45 +0300

    Add project files.

---
+++ Model/Temperature.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApplication.Model
+{
+    public class Temperature
+    {
+        public MeasurementUnit Metric { get; set; }
+        public MeasurementUnit Imperial { get; set; }
+    }
+}
